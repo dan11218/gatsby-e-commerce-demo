50810ffe45fc27fc3309592ef7d2aaf9acc63a09
Yoni Schirris <yoni@usabilla.com>
2019-09-05 18:46:43 +0200

    set up connection with http://yoni-dev-usabilla-blog.webslice.eu/blog

--- usabilla-gatsby/gatsby-config.js
+++ usabilla-gatsby/gatsby-config.js
@@ -43,26 +43,28 @@ module.exports = {
                 // The below URL comes from the Gatsby wordpress source tutorial/
 
                 // baseUrl: `gatsbilla.wordpress.com` <-- this should be the slug once WP has updated.
-                baseUrl: `gatsbilla.wordpress.com`,
-                protocol: `https`,
+                baseUrl: `yoni-dev-usabilla-blog.webslice.eu/blog`,
+                protocol: `http`,
                 // The protocol. This can be http or https.
 
                 // Indicates whether the site is hosted on wordpress.com.
                 // If false, then the assumption is made that the site is self hosted.
                 // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
                 // If your site is hosted on wordpress.org, then set this to false.
-                hostingWPCOM: true,
+                hostingWPCOM: false,
                 // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
                 // This feature is untested for sites hosted on WordPress.com
-                useACF: false,
+                useACF: true,
                 includedRoutes: [
-                    "**/posts",
                     "**/pages",
-                    "**/media",
                     "**/categories",
                     "**/tags",
                     "**/taxonomies",
                     "**/users",
+                ],
+                excludeRoutes: [
+                    "**/posts",
+                    "**/media",
                 ]
             },
         },
