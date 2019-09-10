07bda51bc97da52978ec8bad05e66eb4141a3208
Yoni Schirris <yoni@usabilla.com>
2019-09-10 13:23:03 +0200

    change layout to load new navbar and footer components with some prop logic

--- usabilla-gatsby/src/components/layout.js
+++ usabilla-gatsby/src/components/layout.js
@@ -1,36 +1,16 @@
 import React from "react"
 import { useStaticQuery, Link, graphql } from "gatsby"
+import NavBar from '../components/navbar'
+import Footer from '../components/footer'
 
-const ListLink = props => (
-    <li style={{display: `inline-block`, marginRight: `1rem`}}>
-        <Link to={props.to}>{props.children}</Link>
-    </li>
-)
-export default ({children}) => {
-    const data = useStaticQuery(
-        graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-    )
+export default (props) => {
+
     return (
         <div style={{margin: `3rem auto`, maxWidth: 650, padding: `0 1rem`}}>
-            <header style={{marginBottom: `1.5rem`}}>
-                <Link to="/" style={{textShadow: `none`, backgroundImage: `none`}}>
-                    <h3 style={{display: `inline`}}>{ data.site.siteMetadata.title }</h3>
-                </Link>
-                <ul style={{listStyle: `none`, float: `right`}}>
-                    <ListLink to="/">Home</ListLink>
-                    <ListLink to="/about/">About</ListLink>
-                    <ListLink to="/contact/">Contact</ListLink>
-                </ul>
-            </header>
-            {children}
+            <NavBar></NavBar>
+            {props.children}
+            <Footer footerSize={props.footerSize} nextPage={props.footerNextPage} previousPage={props.footerPrevPage} pageNumber={props.footerPageNumber}></Footer>
+
         </div>
     )
 }
