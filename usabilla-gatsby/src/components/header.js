758852d1d9a6a7a8d02a49378cfd63281556bd66
Yoni Schirris <yoni@usabilla.com>
2019-09-25 09:46:32 +0200

    add bootstrap, refactor global styles with ub- nomenclature

--- usabilla-gatsby/src/components/header.js
+++ usabilla-gatsby/src/components/header.js
@@ -1,3 +1,5 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
 
 export default props => <h1 className={props.class} >{props.headerText}</h1>
