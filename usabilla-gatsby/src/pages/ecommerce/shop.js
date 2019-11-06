d807e8a106fa8ba425d2096b3ff84239e32842cc
Danny Graugnard <dangraug@me.com>
2019-11-06 16:40:51 -0500

    fixes css for list of products, creates search bar

--- usabilla-gatsby/src/pages/ecommerce/shop.js
+++ usabilla-gatsby/src/pages/ecommerce/shop.js
@@ -1,28 +1,31 @@
 import React from "react"
-import {css} from "@emotion/core"
-import {Link, graphql} from "gatsby"
-import {rhythm} from "../../utils/typography"
+// import {css} from "@emotion/core"
+// import {Link, graphql} from "gatsby"
+// import {rhythm} from "../../utils/typography"
 import Header from "../../components/header"
 import Layout from "../../components/layoutProduct"
 import Product from "../../components/productCard"
 import heroImage from "../../../static/assets/Hero_image.png"
 
-// TODO: Work on Header component, make font and color of text modular
-
 export default () => (
     <Layout footerSize='small' footerNextPage='/ecommerce/product' footerPrevPage='/' footerPageNumber='1'>
         <div>
             <div style={styles.jumbotron}>
-                <div style={styles.productHeader}>
+                <div style={styles.header}>
                     <Header class="bold" headerText="E-Commerce Simulation" />
                     <Header class="light" headerText="The online shopping experience" />
                 </div>
             </div>
+            <div>
+            <form style={styles.form}>
+                <label><input style={styles.input} type="text" name="field" placeholder="Search..." /></label><img src={require('../../../static/assets/search.png')} />
+            </form>
+            </div>
             <div className='container'>
                 <div className='row'>
                     <Product image={require('../../../static/assets/coffee.png')} name={"Usabilla Coffee"} description={"Intense Blend"} price={"€18.00"} />
-                    <Product image={require('../../../static/assets/hoodie.png')} name={"Usabilla Hoodie"} description={"Blue | 100% cotton"} price={"€50.00"} />
-                    <Product image={require('../../../static/assets/shirt.png')} name={"Usabilla T-Shirt"} description={"Grey | 100% Cotton"} price={"€30.00"} />
+                    <Product image={require('../../../static/assets/hoodie.png')} name={"Usabilla Hoodie"} description={"Blue | 100% cotton"} price={"€30.00"} />
+                    <Product image={require('../../../static/assets/shirt.png')} name={"Usabilla T-Shirt"} description={"Grey | 100% Cotton"} price={"€25.00"} />
                     <Product image={require('../../../static/assets/canvas-tote-bag.png')} name={"Canvas Bag"} description={"White"} price={"€14.00"} />
                     <Product image={require('../../../static/assets/pen.png')} name={"Pen"} description={"Black | Ballpoint"} price={"€10.00"} />
                     <Product image={require('../../../static/assets/planner.png')} name={"Planner"} description={"Black"} price={"€13.00"} />
@@ -38,7 +41,7 @@ const styles = {
         height: '500px',
         backgroundSize: 'cover',
     },
-    productHeader: {
+    header: {
         margin: '0 30px 0',
         position: 'relative',
         top: '50%',
@@ -47,5 +50,12 @@ const styles = {
         backgroundColor: 'rgba(0, 166, 202, 0.9)',
         padding: '.5rem',
         borderRadius: '15px',
+    },
+    form: {
+        textAlign: 'right',
+    },
+    input: {
+        border: 'none',
+        borderBottom: '2px solid rgb(0, 166, 202)',
     }
 }
